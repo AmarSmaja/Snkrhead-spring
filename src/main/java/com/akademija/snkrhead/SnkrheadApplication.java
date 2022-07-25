edff7016a1feba122910af015134b78bdd44c0ab
Amar Smaic <amarsmaic@gmail.com>
2022-07-25 06:17:41 +0200

    Fresh Spring dir

---
+++ src/main/java/com/akademija/snkrhead/SnkrheadApplication.java
@@ -0,0 +1,13 @@
+package com.akademija.snkrhead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnkrheadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnkrheadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akademija/snkrhead/SnkrheadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akademija.snkrhead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnkrheadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
